9c21ebcfa393841b54e974c2c3443c8d099c61f6
Blackthe123 <71077658+Blackthe123@users.noreply.github.com>
2020-11-07 09:44:27 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,24 @@
+class DustBin {  
+    constructor( ){
+        var options = {
+            isStatic : true
+        }
+
+        this.bodyb = Bodies.rectangle(650, 665, 120, 20, options);
+	    World.add(world, this.bodyb);
+	    this.bodyl= Bodies.rectangle(580, 605, 20, 140,options);
+	    World.add(world, this.bodyl);
+	    this.bodyr = Bodies.rectangle(720, 605, 20, 140,options);
+        World.add(world, this.bodyr);
+        this.bodyb.width = width
+        this.bodyb.height = height 
+        
+   }
+   display(){
+       fill("white");
+       rectMode(CENTER);
+       rect(650, 665, 120, 20);
+       rect(580, 605, 20, 140);
+       rect(720, 605, 20, 140);
+   }
+}
